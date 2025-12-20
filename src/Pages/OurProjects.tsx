07f1563cd7c304c20a4358f902e6616dde1f2a5c
MitM123 <mitmonpara23@gmail.com>
2025-12-21 01:25:57 +0530

    FIX

--- src/Pages/OurProjects.tsx
+++ src/Pages/OurProjects.tsx
@@ -31,7 +31,7 @@ const OurProjects = () => {
               WHO WE ARE
             </motion.p> */}
             <motion.h1
-              className="text-6xl lg:text-7xl font-bold mt-8 lg:mt-8 text-white leading-tight mb-6"
+              className="text-[55px] lg:text-7xl font-bold mt-8 lg:mt-8 text-white leading-tight mb-6"
               initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.5 }}
